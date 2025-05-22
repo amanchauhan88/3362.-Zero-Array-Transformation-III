cde7bb6f27b54f6a944ccb895e44020043e38375
Aman Chauhan <amanchauhan88580@gmail.com>
2025-05-22 13:03:36 +0530

    Some changes

--- Solution.java
+++ Solution.java
@@ -1,3 +1,10 @@
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue; 
+import java.util.Queue;
+
 class Solution {
   public int maxRemoval(int[] nums, int[][] queries) {
     int queryIndex = 0;
